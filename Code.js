0c5d21210deee00d6afcb5ed040e40c2c6d2a916
jesse <jesse.hathaway@getbraintree.com>
2015-10-02 13:34:40 -0500

    Allow more than one message consumer
    Initial attempt at allowing arbitrary message consumers.

--- Code.js
+++ Code.js
@@ -1,23 +1,90 @@
 function main() {
+  var i;
+  var msgConsumers = [{
+    name: "Top Senders",
+    db: null,
+    ssID: null,
+    initSpreadSheet: function () {
+      var ss = SpreadsheetApp.openById(this.ssID);
+      var sheet = ss.insertSheet(this.name);
+      sheet.appendRow(["From", "Count"]);
+    },
+    msgFunction: function (msg) {
+      var rawFrom = msg.getFrom();
+      var from = extractEmailAddress(rawFrom);
+      var rows = objDB.getRows(this.db, this.name, ['From','Count'], {From:from});
+      if ( rows.length === 0) {
+        objDB.insertRow(this.db, this.name, {From:from, Count:1} );
+      } else {
+        var newCount = rows[0].Count + 1;
+        objDB.updateRow(this.db, this.name, {From:from, Count:newCount}, {From:from} );
+      }
+    },
+    graphFunction: function () {
+      var ss = SpreadsheetApp.openById(this.ssID);
+      var sheetID = ss.getSheetByName(this.name).getSheetId();
+      var encodedQuery = encodeURIComponent("select A,B order by B desc limit 10");
+      var dataSourceURL = 'https://docs.google.com/spreadsheet/tq?key=' + this.ssID;
+      dataSourceURL += '&tq=' + encodedQuery;
+      dataSourceURL += '&gid=' + sheetID;
+      dataSourceURL += '&headers=-1';
+      var chartBuilder = Charts.newBarChart()
+      .setTitle('Top Senders')
+      .setXAxisTitle('Count')
+      .setYAxisTitle('Sender')
+      .setDimensions(1024, 768)
+      .setOption('hAxis.format', {format: 'decimal'})
+      .setOption('chartArea', {left: '40%'})
+      .setDataSourceUrl(dataSourceURL);
+      var chart = chartBuilder.build();
+      return chart.getAs('image/png').setName("chart.png");
+    },
+  }
+  ];
+
+  var dataPopulated = false;
   var userProperties = PropertiesService.getUserProperties();
+  var ssID = userProperties.getProperty("ssID");
 
-  ssID = userProperties.getProperty('ssID');
-
-  if ( ssID === null ) {
+  if (ssID === null) {
     Logger.log("Spreadsheet ID not set, creating a new one");
     ssInfo = createSpreadSheet(userProperties);
     ssID = userProperties.getProperty('ssID');
-    populateSpreadSheet(ssID);
     // This is needed so we can use the Google Visualization API Query Language
     // to set the data source URL
     allowPublicViewing(ssID);
+  } else {
+    dataPopulated = true;
   }
 
-  Logger.log('ssID' + ssID);
-  Logger.log('Drawing Bar Chart');
-  chartBlob = drawChart(ssID);
+  var ss = SpreadsheetApp.openById(ssID);
+  var db = objDB.open(ssID);
+
+  for(i = 0; i < msgConsumers.length; i += 1) {
+    msgConsumers[i].db = db;
+    msgConsumers[i].ssID = ssID;
+  }
+  
+  if (! dataPopulated) {
+    for(i = 0; i < msgConsumers.length; i += 1) {
+      msgConsumers[i].initSpreadSheet();
+    }
+    searchGmail(msgConsumers);
+  }
+
+  var chartBlobs = [];
+  for(i = 0; i < msgConsumers.length; i += 1) {
+    chartBlobs.push(msgConsumers[i].graphFunction());
+  }
+
   Logger.log('Sending Email');
-  sendEmail(chartBlob);
+  for(i = 0; i < chartBlobs.length; i += 1) {
+    sendEmail(chartBlobs[i]);
+  }
+
+  if (ss.getSheetByName("Sheet1") !== null) {
+    ss.deleteSheet(ss.getSheetByName("Sheet1"));
+  }
 }
 
 function clearUserProperties() {
@@ -28,30 +95,6 @@ function clearUserProperties() {
 function extractEmailAddress(emailAddress) {
   emailAddressRegex = /^(?:.*<)?([^<>]+)(?:>)?$/;
   return emailAddressRegex.exec(emailAddress)[1].toLowerCase();
-}
-
-function drawChart(ssID) {
-  encodedQuery = encodeURIComponent("select A,B order by B desc limit 10");
-  dataSourceUrl = 'https://docs.google.com/spreadsheet/tq?key=' + ssID + '&tq=' + encodedQuery + '&headers=-1';
-  var chartBuilder = Charts.newBarChart()
-  .setTitle('Top Senders')
-  .setXAxisTitle('Count')
-  .setYAxisTitle('Sender')
-  .setDimensions(1024, 768)
-  .setOption('hAxis.format', {format: 'decimal'})
-  .setOption('chartArea', {left: '40%'})
-  .setDataSourceUrl(dataSourceUrl);
-
-  // .setLegendPosition(Charts.Position.NONE)
-  // .setOption('fontSize', 10)
-
-  // var chartBuilder = Charts.newPieChart()
-  // .setTitle('Top Recepients')
-  // .setDimensions(1024, 768)
-  // .setDataSourceUrl('https://docs.google.com/spreadsheet/tq?key=' + ssID + '&tq=select%20A%2CB%20order%20by%20B%20desc%20limit%205&headers=-1');
-
-  var chart = chartBuilder.build();
-  return chart.getAs('image/png').setName("chart.png");
 }
 
 function sendEmail(chartBlob) {
@@ -72,18 +115,12 @@ function allowPublicViewing(ID) {
 }
 
 function createSpreadSheet(userProperties) {
-  var spreadsheet_new = SpreadsheetApp.create("email email email - " + Date());
-
-  ssID = spreadsheet_new.getUrl().split('/')[7];
+  var ssNew = SpreadsheetApp.create("email email email - " + Date());
+  var ssID = ssNew.getUrl().split('/')[7];
   userProperties.setProperty('ssID', ssID);
-
-  var ss = SpreadsheetApp.openById(ssID);
-  var sheet = ss.getSheets()[0];
-  sheet.appendRow(["From", "Count"]);
 }
 
-function populateSpreadSheet(ssID) {
-  var db = objDB.open(ssID);
+function searchGmail(msgConsumers) {
   var batchSize = 10;
   // Process up to 100 gmail threads at once
   for (j = 0; j < 100; j+=batchSize) {
@@ -91,15 +128,9 @@ function populateSpreadSheet(ssID) {
     var threads = GmailApp.search('after:2015/07/30 before:2015/08/01', j, batchSize);
     Logger.log('Search Complete: ' + threads.length);
     for (thread = 0; thread < threads.length; thread += 1) {
-      var messages = threads[thread].getMessages();
-      var rawFrom = messages[0].getFrom();
-      var from = extractEmailAddress(rawFrom);
-      var rows = objDB.getRows( db, 'Sheet1', ['From','Count'], {From:from} );
-      if ( rows.length === 0) {
-        objDB.insertRow( db, 'Sheet1', {From:from, Count:1} );
-      } else {
-        var newCount = rows[0].Count + 1;
-        objDB.updateRow( db, 'Sheet1', {From:from, Count:newCount}, {From:from} );
+      var msg = threads[thread].getMessages()[0];
+      for(var i = 0; i < msgConsumers.length; i += 1) {
+        msgConsumers[i].msgFunction(msg);
       }
     }
     Logger.log('Batch: ' + j);
