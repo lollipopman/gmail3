400325c87b9f17abfcbebf0cf8099a33df570e40
Jesse Hathaway <jesse@mbuki-mvuki.org>
2015-12-04 08:58:39 -0600

    Add some comments to the main functions

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
+/**
+ * Used to start the script. Adds a daily trigger and then begins to process
+ * messages. Once it is through processing a batch of messages it will schedule
+ * the next trigger. 
+ */
 function main() {
   gmail3AddMsgConsumers();
   gmail3.main();
 }
 
+/**
+ * Enables more verbose logging and only processes 20 messages per run. Also
+ * restricts the total messages processed per month to 40.
+ */
 function enableDebugMode() {
   PropertiesService.getUserProperties().setProperty("debugEnabled", "true");
   Logger.log("Debugging enabled");
 }
 
+/** Disables debug mode */
 function disableDebugMode() {
   PropertiesService.getUserProperties().setProperty("debugEnabled", "false");
   Logger.log("Debugging disabled");
 }
 
+/**
+ * Deletes all existing gmail3 Google sheets, and deletes all triggers. This
+ * effectively disables the script.
+ */
 function resetScriptState() {
   var userProperties = PropertiesService.getUserProperties();
   userProperties.deleteAllProperties();
